f5394422c3a51511f2ee5b8d419a3aac23f5cb62
error0 <error0@yeah.net>
2022-06-19 12:16:25 +0800

    事务回滚，页面回收相关

--- src/bolt/freelist.go
+++ src/bolt/freelist.go
@@ -9,8 +9,11 @@ import (
 // freelist represents a list of all pages that are available for allocation.
 // It also tracks pages that have been freed but are still in use by open transactions.
 type freelist struct {
-	ids     []pgid          // all free and available free page ids.
-	pending map[txid][]pgid // mapping of soon-to-be free page ids by tx.  即将释放事务page
+	ids []pgid // all free and available free page ids.
+	// 写事务把Node转换Page时会创建新的Page，原因是防止旧事务读取到，然后旧的page的id可以释放掉旧丢在pending
+	// 等待下次写事务 db.freelist.release(minid - 1)。 写事务回滚时，会把对应事务的pgid删除
+	//回收页只是把pgid放进ids(空闲id)
+	pending map[txid][]pgid // mapping of soon-to-be free page ids by tx.
 	cache   map[pgid]bool   // fast lookup of all free and pending page ids.
 }
 
